59abeb0f5408e316da4f4827447981b9e0071066
K <oriolum@gmail.com>
2025-07-27 08:14:35 -0400

    Update admin.php

--- admin.php
+++ admin.php
@@ -307,7 +307,7 @@ $bankFunds = $pdo->query("SELECT total_funds FROM fund_bank WHERE id = 1")->fetc
 <?php endforeach; ?>
 
 <footer>
-  &copy; 2023 [j3]
+  &copy; 2025 [j3]
   <a href="https://github.com/artsystema/wbt" target="_blank">
     <img src="assets/icons/github.svg" alt="GitHub">
   </a>
